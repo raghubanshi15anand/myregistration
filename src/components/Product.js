da739da43f8f01ea340d85e5b5082c2afd8f7b5e
anand <raghubanshi15anand@gmail.com>
2022-07-31 23:03:34 -0700

    commited changes in css

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,13 +7,13 @@ import './Product.css';
 const Product = () => {
 
   return (  
-    <div>  
+    <div class="all-container">  
     <div class="card"> 
       <div class="card-image"></div>
       <div class="card-text">
         <span class="date"></span>
         <h2>	Galaxy F13 	</h2>
-        <p>₹12999.00</p>
+        <h3>₹12999.00</h3>
       </div>
     </div>
 
@@ -22,7 +22,7 @@ const Product = () => {
       <div class="card-text card2">
         <span class="date"> </span>
         <h2>Apple iPhone 13 Pro Max (512GB) - Gold</h2>
-        <p> ₹1,55,900.00 </p>
+        <h3> ₹1,55,900.00 </h3>
       </div>     
     </div>
 
@@ -31,7 +31,7 @@ const Product = () => {
         <div class="card-text card3">
           <span class="date"> </span>
           <h2> OPPO A31 (Mystery Black, 6GB RAM, 128GB Storage) </h2>
-          <p> ₹11,990.00 </p>
+          <h3> ₹11,990.00 </h3>
         </div>        
       </div>
 
@@ -39,8 +39,8 @@ const Product = () => {
         <div class="card-image card4"></div>
         <div class="card-text card3">
           <span class="date"></span>
-          <h2> Low Cost </h2>
-          <p> Monitor all the sensitive information of your machines at an minimal cost. </p>
+          <h2> SONY XPERIA XA1 DUAL BLACK 32GB </h2>
+          <h3> ₹10,996.00 </h3>
         </div>
       </div>
 
@@ -48,8 +48,8 @@ const Product = () => {
         <div class="card-image card5"></div>
         <div class="card-text card3">
           <span class="date"></span>
-          <h2>Predictive AI</h2>
-          <p>Leverage the AI algorithms to detect occurrence of error.</p>
+          <h2>Motorola G31 128 GB, 6 GB RAM, Meteorite Gray</h2>
+          <h3>13,999.00.</h3>
         </div>       
       </div>
 
@@ -57,8 +57,8 @@ const Product = () => {
         <div class="card-image card6"></div>
         <div class="card-text card3">
           <span class="date"></span>
-          <h2>Multiple Asset monitoring</h2>
-          <p>Track multiple assets in a common place</p>
+          <h2>New Original HTC Desire 10 Pro 4GB RAM 64GBg</h2>
+          <h3>₹31,617</h3>
         </div>       
       </div>
     </div>
